ba766a07b879dde9808e9f13f7f65e7a734e5763
willjejones <will.je.jones@hotmail.com>
2015-02-06 13:41:16 +0000

    Calendar
    Added calendar to index page

--- index.php
+++ index.php
@@ -4,8 +4,50 @@
         <title>WCT Tool | Home</title>
         <link rel="icon" type="image/png" href="./img/wct_icon.png">
         <link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
+		<link href='css/fullcalendar.css' rel='stylesheet' />
+		<link href='css/fullcalendar.print.css' rel='stylesheet' media='print' />
+		<script src='calendar_api/lib/moment.min.js'></script>
+		<script src='calendar_api/lib/jquery.min.js'></script>
+		<script src='calendar_api/fullcalendar.min.js'></script>
+		<script>
+
+			$(document).ready(function() {
+			
+				var newEvents = loadEvents();
+				var jsoEvents = eval(newEvents);
+			
+				var today = new Date();
+				var dd = today.getDate();
+				var mm = today.getMonth()+1; //January is 0!
+				var yyyy = today.getFullYear();
+
+				if(dd<10) {
+					dd='0'+dd
+				} 
+
+				if(mm<10) {
+					mm='0'+mm
+				} 
+
+				today = yyyy+'-'+mm+'-'+dd;
+				
+				$('#calendar').fullCalendar({
+					header: {
+						left: 'prev,next today',
+						center: 'title',
+						right: 'month,agendaWeek,agendaDay'
+					},
+					defaultDate: today,
+					editable: true,
+					eventLimit: true, // allow "more" link when too many events
+					events: jsoEvents
+				});
+				
+			});
+
+		</script>
     </head>
-    <body>
+    <body onload="loadJourneys()">
         <div id="wctLogo"></div>
         <nav>
             <ul>
@@ -13,7 +55,7 @@
                 <li><a href="journeys.html">Journeys</a>
                     <ul>
                         <li><a href="manage_journeys.php">Manage Journeys</a></li>
-                        <li><a href="add_journey.php">Add Journeys</a></li>
+                        <li><a href="add_journeys.php">Add Journeys</a></li>
                     </ul>
                 </li>
                 <li><a href="groups.html">Groups</a>
@@ -45,5 +87,33 @@
         <div id="main">
             Insert content here
         </div>
+		<script>
+	
+			//Need to get stuff from database!
+			function loadEvents() {		
+				return "[	{		title: 'WI to Quiz Night',		start: '2015-02-03',		url: 'showJourneyInfo.php'	},	{		title: 'Shopping Trip',		start: '2015-02-09T11-00',		end: '2015-02-09T13-00', url: 'showJourneyInfo.php'	}]";	
+			}
+			
+			function loadJourneys() {
+			
+				$.ajax({
+                    type: "POST",
+                    url:"MySQL_Functions.php",
+                    data: {
+                        'form_type': 'getJourneys'
+                    },
+                    dataType: "json",
+                    success: function(returned_data) {
+                        alert(JSON.stringify(returned_data));
+                    }
+                });
+			
+			}
+			
+			
+		
+		</script>
+	<div id='cTitle'><br><br>Journey Overview</div>
+	<div id='calendar'></div>
     </body>
 </html>
