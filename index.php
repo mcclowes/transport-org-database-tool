cf187c990e2e2256296f633fb9b47453136ab86e
Max Clayton Clowes <maxcc@me.com>
2015-02-16 11:25:20 +0000

    Consistent Navigation Added

--- index.php
+++ index.php
@@ -12,41 +12,7 @@
     </head>
     <body onload="loadJourneys()">
         <div id="wctLogo"></div>
-        <nav>
-            <ul>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="journeys.php">Journeys</a>
-                    <ul>
-                        <li><a href="journeys.php">Manage Journeys</a></li>
-                        <li><a href="add_journey.php">Add Journey</a></li>
-                    </ul>
-                </li>
-                <li><a href="groups.php">Groups</a>
-                    <ul>
-                        <li><a href="manage_groups.php">Manage Groups</a></li>
-                        <li><a href="add_group.php">Add group</a></li>
-                    </ul>
-                </li>
-                <li><a href="members.php">Members</a>
-                    <ul>
-                        <li><a href="manage_members.php">Manage Members</a></li>
-                        <li><a href="add_member.php">Add Member</a></li>
-                    </ul>
-                </li>
-                <li><a href="vehicles.php">Vehicles</a>
-                    <ul>
-                        <li><a href="manage_vehicles.php">Manage Vehicles</a></li>
-                        <li><a href="add_vehicle.php">Add vehicle</a></li>
-                    </ul>
-                </li>
-                <li><a href="drivers.php">Drivers</a>
-                    <ul>
-                        <li><a href="manage_drivers.php">Manage Drivers</a></li>
-                        <li><a href="add_driver.php">Add Driver</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </nav>
+        <?php include 'nav.php' ?>
         <div id="main">
             Insert content here
         </div>
--- journeys.php
+++ journeys.php
@@ -5,41 +5,7 @@
         <link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
     </head>
     <body>
-        <nav>
-            <ul>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="journeys.html">Journeys</a>
-                    <ul>
-                        <li><a href="journeys.php">Manage Journeys</a></li>
-                        <li><a href="add_journey.php">Add Journey</a></li>
-                    </ul>
-                </li>
-                <li><a href="groups.html">Groups</a>
-                    <ul>
-                        <li><a href="manage_groups.php">Manage Groups</a></li>
-                        <li><a href="add_group.php">Add group</a></li>
-                    </ul>
-                </li>
-                <li><a href="members.html">Members</a>
-                    <ul>
-                        <li><a href="manage_members.php">Manage Members</a></li>
-                        <li><a href="add_member.php">Add Member</a></li>
-                    </ul>
-                </li>
-                <li><a href="vehicles.html">Vehicles</a>
-                    <ul>
-                        <li><a href="manage_vehicles.php">Manage Vehicles</a></li>
-                        <li><a href="add_vehicle.php">Add vehicle</a></li>
-                    </ul>
-                </li>
-                <li><a href="drivers.html">Drivers</a>
-                    <ul>
-                        <li><a href="manage_drivers.php">Manage Drivers</a></li>
-                        <li><a href="add_driver.php">Add Driver</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </nav>
+        <?php include 'nav.php' ?>
         <div id="main">
             Insert content here
         </div>
