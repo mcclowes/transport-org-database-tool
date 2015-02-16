25a023bc329993895bc75457a2392e66108661e0
keyead <edw.key@gmail.com>
2015-02-16 14:12:25 +0000

    See last submit

--- MySQL_Functions.php
+++ MySQL_Functions.php
@@ -502,16 +502,17 @@ function getJourneys($mysqli){
 
 function getJourney($mysqli, $Journey_ID){
 	$rdata = array();
-	if($statement = $mysqli->prepare(" SELECT Journey_ID, Journey_Description, Booking_Date, fName, sName, Address_ID, Tel_No, Group_ID, Journey_Date, Destination, Return_Note, Return_Time,
+	if($statement = $mysqli->prepare(" SELECT Journey_ID, Journey_Description, Journey_Note, Booking_Date, fName, sName, Address_ID, Tel_No, Group_ID, Journey_Date, Destination, Return_Note, Return_Time,
 										No_Passengers, Passengers_Note, Wheelchairs, Transferees, Other_Access, Booked_By, Driver_ID, Vehicle, 
-										Keys_To_Collect, Distance, Quote, Invoice_Sent, Invoice_Paid FROM  Journeys WHERE Journey_ID = ?;")){
+										Keys_To_Collect, Distance, Quote, Invoice_Cost, Invoice_Sent, Invoice_Paid FROM  Journeys WHERE Journey_ID = ?;")){
 		$statement->bind_param("i", $Journey_ID);
 		$statement->execute();
 		$statement->store_result();
-		$statement->bind_result($rdata['Journey_ID'], $rdata['Journey_Description'], $rdata['Booking_Date'],$rdata['fName'],$rdata['sName'], $Address_ID1, $rdata['Tel_No'], $rdata['Group_ID'],$rdata['Journey_Date'], $Address_ID2, $rdata['Return_Note'], $rdata['Return_Time'], 
+		$statement->bind_result($rdata['Journey_ID'], $rdata['Journey_Description'], $rdata['Journey_Note'], $rdata['Booking_Date'],$rdata['fName'],$rdata['sName'], $Address_ID1, $rdata['Tel_No'], $rdata['Group_ID'],$rdata['Journey_Date'], $Address_ID2, $rdata['Return_Note'], $rdata['Return_Time'], 
 								$rdata['No_Passengers'], $rdata['Passengers_Note'], $rdata['Wheelchairs'], $rdata['Transferees'], $rdata['Other_Access'], $rdata['Booked_By'], $rdata['Driver_ID'], $rdata['Vehicle'], 
 								$rdata['Keys_To_Collect'], $rdata['Distance_Run'], $rdata['Quote'], $rdata['Invoice_Cost'], $rdata['Invoice_Sent'], $rdata['Invoice_Paid']);
 		$statement->fetch();
+		$statement->close();
 	}
 	$rdata['Booking_Date'] = outputDate($rdata['Booking_Date']);
 	$rdata['Jouney_Date'] = outputDate($rdata['Jouney_Date']);
