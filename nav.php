3299f152db07e2f57d34e7796297785038b52876
Max Clayton Clowes <maxcc@me.com>
2015-02-16 15:11:26 +0000

    Nav done

--- nav.php
+++ nav.php
@@ -1,7 +1,7 @@
 <nav>
     <ul>
         <li><a href="index.php">Home</a></li>
-        <li><a href="group_journeys.php">Journeys</a>
+        <li><a href="index.php">Journeys</a>
             <ul>
                 <li><a href="group_journeys.php">View Group Journeys</a></li>
                 <li><a href="add_group_journey.php">Add Group Journey</a></li>
@@ -9,7 +9,7 @@
                 <li><a href="add_TC_journey.php">Add Travel Club Journey</a></li>
             </ul>
         </li>
-        <li><a href="group_journeys.php">Groups</a>
+        <li><a href="add_group.php">Groups</a>
             <ul>
                 <li><a href="manage_groups.php">Manage Groups</a></li>
                 <li><a href="add_group.php">Add Group or Organisation</a></li>
@@ -17,7 +17,7 @@
                 <li><a href="forms/Group_Registration_Form.pdf" download>Group Registration Form</a></li>
             </ul>
         </li>
-        <li><a href="TC_journeys.php">Travel Club</a>
+        <li><a href="add_member.php">Travel Club</a>
             <ul>
                 <li><a href="manage_members.php">Manage Members</a></li>
                 <li><a href="add_member.php">Add Travel Club Member</a></li>
@@ -25,13 +25,13 @@
                 <li><a href="forms/Individual_Registration_Form.pdf" download>Member Registration Form</a></li>
             </ul>
         </li>
-        <li><a href="manage_vehicles.php">Vehicles</a>
+        <li><a href="add_vehicle.php">Vehicles</a>
             <ul>
                 <li><a href="manage_vehicles.php">Manage Vehicles</a></li>
                 <li><a href="add_vehicle.php">Add vehicle</a></li>
             </ul>
         </li>
-        <li><a href="manage_drivers.php">Drivers</a>
+        <li><a href="add_driver.php">Drivers</a>
             <ul>
                 <li><a href="manage_drivers.php">Manage Drivers</a></li>
                 <li><a href="add_driver.php">Add Driver</a></li>
