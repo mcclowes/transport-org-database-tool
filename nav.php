d706b1e8944349a82ba5dda81b097ddd2e7ec5e7
Max Clayton Clowes <maxcc@me.com>
2015-02-16 11:39:27 +0000

    Navigation and Forms finished

--- nav.php
+++ nav.php
@@ -5,19 +5,22 @@
             <ul>
                 <li><a href="journeys.php">Manage Journeys</a></li>
                 <li><a href="add_journey.php">Add Journey</a></li>
+                <li><a href="/forms/Group_Booking_Form.pdf" download>Group Booking Form</a></li>
+                <li><a href="/forms/Individual_Booking_Form.pdf" download>Individual Booking Form</a></li>
             </ul>
         </li>
         <li><a href="groups.php">Groups</a>
             <ul>
                 <li><a href="manage_groups.php">Manage Groups</a></li>
                 <li><a href="add_group.php">Add group</a></li>
-                <li><a href="group_forms.pdf" download>Download Group Form</a></li>
+                <li><a href="/forms/Group_Registration_Form.pdf" download>Group Registration Form</a></li>
             </ul>
         </li>
         <li><a href="members.php">Members</a>
             <ul>
                 <li><a href="manage_members.php">Manage Members</a></li>
                 <li><a href="add_member.php">Add Member</a></li>
+                <li><a href="/forms/Individual_Registration_Form.pdf" download>Member Registration Form</a></li>
             </ul>
         </li>
         <li><a href="vehicles.php">Vehicles</a>
